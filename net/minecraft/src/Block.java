c24c399560ea2e1b7d738a321edd3fd6645e5ef4
Tori Hall <144717874+Toriiiiiiiiii@users.noreply.github.com>
2025-03-30 14:54:25 +0100

    Add flint bricks & double render distances

--- net/minecraft/src/Block.java
+++ net/minecraft/src/Block.java
@@ -120,6 +120,7 @@ public class Block {
 	public static final Block stairCompactSmoothBrick = new BlockStairs(98, brickSmooth);
 	public static final Block blockCopper = (new Block(99, 7+3*16, Material.iron)).setHardness(5.0F).setResistance(10.0F).setStepSound(soundMetalFootstep);
 	public static final Block leafPile = (new BlockLeafPile(100, 14+8*16)).setHardness(0.1F).setStepSound(soundGrassFootstep);
+	public static final Block blockFlint = (new Block(101, Block.brickStone.blockIndexInTexture + 16, Material.rock)).setHardness(2.0F).setResistance(10.0F).setStepSound(soundStoneFootstep);
 	public int blockIndexInTexture;
 	public final int blockID;
 	protected float hardness;
