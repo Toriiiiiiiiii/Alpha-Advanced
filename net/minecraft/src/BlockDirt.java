14c568cfa1d2c47faec4eb378cef98350bf0a686
Tori Hall <144717874+Toriiiiiiiiii@users.noreply.github.com>
2025-04-03 22:29:53 +0100

    Using fertilizer on dirt will now grow grass

--- net/minecraft/src/BlockDirt.java
+++ net/minecraft/src/BlockDirt.java
@@ -4,4 +4,14 @@ public class BlockDirt extends Block {
 	protected BlockDirt(int var1, int var2) {
 		super(var1, var2, Material.grass);
 	}
+	
+	public boolean blockActivated(World var1, int var2, int var3, int var4, EntityPlayer var5) {		
+		ItemStack currentItem = var5.inventory.getCurrentItem();
+		if(currentItem != null && currentItem.itemID == Item.fertilizer.shiftedIndex) {
+			var1.setBlockWithNotify(var2, var3, var4, Block.grass.blockID);
+			var5.inventory.getCurrentItem().stackSize--;
+		}
+		
+		return true;
+	}
 }
