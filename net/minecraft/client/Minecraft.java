2f84294491e6f26102d37c7e32743aee044e7b12
Tori Hall <144717874+Toriiiiiiiiii@users.noreply.github.com>
2025-04-10 13:46:18 +0100

    Update version number

--- net/minecraft/client/Minecraft.java
+++ net/minecraft/client/Minecraft.java
@@ -78,7 +78,7 @@ import org.lwjgl.util.glu.GLU;
 
 public abstract class Minecraft implements Runnable {
 	public byte versionMajor    = 1;
-	public byte versionMinor    = 1;
+	public byte versionMinor    = 2;
 	public byte versionSubMinor = 0;
 	
 	public boolean hideUI = false;
