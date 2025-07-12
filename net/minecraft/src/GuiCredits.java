a102c862b00ab82f3056e9c9e385f45d9648318b
Tori Hall <144717874+Toriiiiiiiiii@users.noreply.github.com>
2025-07-12 17:26:20 +0100

    Update credits.

--- net/minecraft/src/GuiCredits.java
+++ net/minecraft/src/GuiCredits.java
@@ -29,20 +29,17 @@ public class GuiCredits extends GuiScreen {
 	public void initButtons() {
 		this.controlList.add(new GuiButton(0, this.width / 2 - 100, this.height / 6 + 168, "Back"));
 		
-		this.controlList.add(new GuiButton(1, this.width / 2 - 100, this.height / 6, "@transgendernerd - Lead Developer"));
-		this.controlList.add(new GuiButton(2, this.width / 2 - 100, this.height / 6 + 24, "@retrorandom - Contributor"));
-		this.controlList.add(new GuiButton(3, this.width / 2 - 100, this.height / 6 + 2*24, "@i3riced - Contributor"));
-		this.controlList.add(new GuiButton(4, this.width / 2 - 100, this.height / 6 + 3*24, "@mongster83 - Art"));
-		
-		this.controlList.add(new GuiButton(5, this.width / 2 - 100, this.height / 6 + 5*24, "Discord Server Mods"));
-		this.controlList.add(new GuiButton(6, this.width / 2 - 100, this.height / 6 + 6*24, "You, the player"));
+		this.controlList.add(new GuiButton(1, this.width / 2 - 100, this.height / 6, "Tori - Lead Developer"));
+		this.controlList.add(new GuiButton(2, this.width / 2 - 100, this.height / 6 + 24, "retrorandom - Contributor"));
+		this.controlList.add(new GuiButton(3, this.width / 2 - 100, this.height / 6 + 2*24, "i3riced - Contributor"));
+		this.controlList.add(new GuiButton(4, this.width / 2 - 100, this.height / 6 + 3*24, "ChimneyEgg - Contributor"));
+		this.controlList.add(new GuiButton(5, this.width / 2 - 100, this.height / 6 + 4*24, "Mongster - Art"));
 		
 		((GuiButton)this.controlList.get(1)).enabled = false;
 		((GuiButton)this.controlList.get(2)).enabled = false;
 		((GuiButton)this.controlList.get(3)).enabled = false;
 		((GuiButton)this.controlList.get(4)).enabled = false;
 		((GuiButton)this.controlList.get(5)).enabled = false;
-		((GuiButton)this.controlList.get(6)).enabled = false;
 	}
 
 	protected void actionPerformed(GuiButton var1) {
@@ -54,7 +51,6 @@ public class GuiCredits extends GuiScreen {
 	public void drawScreen(int var1, int var2, float var3) {
 		this.drawDefaultBackground();
 		this.drawCenteredString(this.fontRenderer, this.screenTitle, this.width / 2, 20, 16777215);
-		this.drawCenteredString(this.fontRenderer, "Special Thanks", this.width / 2, this.height / 6 + 9*12, 16777215);
 		super.drawScreen(var1, var2, var3);
 	}
 }
